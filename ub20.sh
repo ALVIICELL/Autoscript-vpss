131263bd36508b20413f02fe0999d77d9b02fce6
rullpqh <114976790+rullpqh@users.noreply.github.com>
2022-12-06 06:38:52 +0700

    Update ub20.sh
    Signed-off-by: rullpqh <114976790+rullpqh@users.noreply.github.com>

--- ub20.sh
+++ ub20.sh
@@ -218,7 +218,9 @@ cat > /etc/rc.local <<-END
 # rc.local
 # By default this script does nothing.
 iptables -I INPUT -p udp --dport 5300 -j ACCEPT
+iptables -I INPUT -p udp --dport 5400 -j ACCEPT
 iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
+iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5400
 exit 0
 END
 chmod +x /etc/rc.local

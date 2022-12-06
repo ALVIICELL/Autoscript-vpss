90a19dccce5a13848905242774e0a6e4e7134de8
rullpqh <114976790+rullpqh@users.noreply.github.com>
2022-12-07 01:40:39 +0700

    Update ub20.sh
    Signed-off-by: rullpqh <114976790+rullpqh@users.noreply.github.com>

--- ub20.sh
+++ ub20.sh
@@ -7,52 +7,51 @@
 # //  telegram: https://t.me/bhoikfost_yahya
 # //====================================================
 clear
-fun_bar() {
-    commad[0]="$1"
-    commad[1]="$2"
-    (
-        [[ -e $HOME/fim ]] && rm $HOME/fim
-        ${commad[0]} -y >/dev/null 2>&1
-        ${commad[1]} -y >/dev/null 2>&1
-        touch $HOME/fim
-    ) >/dev/null 2>&1 &
-    tput civis
-    echo -ne "  \033[1;33mWAIT \033[1;37m- \033[1;33m["
-    while true; do
-        for ((i = 0; i < 18; i++)); do
-            echo -ne "\033[1;31m#"
-            sleep 0.1s
-        done
-        [[ -e $HOME/fim ]] && rm $HOME/fim && break
-        echo -e "\033[1;33m]"
-        sleep 1s
-        tput cuu1
-        tput dl1
-        echo -ne "  \033[1;33mWAIT \033[1;37m- \033[1;33m["
-    done
-    echo -e "\033[1;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
-    tput cnorm
-}
-
-fun_attt() {
-    apt-get update && update-grub -y
-    apt-get install screen -y
-}
-fun_att() {
-    apt install ncurses-utils -y
-    apt-get install git -y
-    apt install golang -y 
-    apt install dropbear -y 
-}
-fun_at() {
-    apt install msmtp-mta ca-certificates bsd-mailx -y 
-    wget --no-check-certificate https://sc-xray.yha.my.id/fightertunnel && chmod +x fightertunnel >>/dev/null 2>&1
-   
-}
-echo ""
-fun_bar 'fun_attt'
-clear
-fun_bar 'fun_att'
-clear
-fun_bar 'fun_at'
+REQUIRED_UPDATE="update"
+PKG_UPDATE=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_UPDATE | grep "installed ok")
+if [ "" = "$PKG_UPDATE" ]; then
+    apt-get -qq update $REQUIRED_UPDATE -y &>/dev/null
+fi
+REQUIRED_GRUP="GRUP"
+PKG_GRUP=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_GRUP | grep "installed ok")
+if [ "" = "$PKG_GRUP" ]; then
+    apt-get -qq update-grub -y &>/dev/null
+fi
+REQUIRED_CURL="curl"
+PKG_CURL=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_CURL | grep "installed ok")
+if [ "" = "$PKG_CURL" ]; then
+    apt-get -qq install $REQUIRED_CURL -y &>/dev/null
+fi
+REQUIRED_WGET="wget"
+PKG_WGET=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_WGET | grep "installed ok")
+if [ "" = "$PKG_WGET" ]; then
+    apt-get -qq install $REQUIRED_WGET -y &>/dev/null
+fi
+REQUIRED_DNS="dnsutils"
+PKG_DNS=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_DNS | grep "installed ok")
+if [ "" = "$PKG_DNS" ]; then
+    apt-get -qq install $REQUIRED_DNS -y &>/dev/null
+fi
+REQUIRED_SCREEN="screen"
+PKG_SCREEN=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_SCREEN | grep "installed ok")
+if [ "" = "$PKG_SCREEN" ]; then
+    apt-get -qq install $REQUIRED_SCREEN -y &>/dev/null
+fi
+REQUIRED_DP="dropbear"
+PKG_DP=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_DP | grep "installed ok")
+if [ "" = "$PKG_DP" ]; then
+    apt-get -qq install $REQUIRED_DP -y &>/dev/null
+fi
+REQUIRED_NCT="ncurses-utils"
+PKG_NCT=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_NCT | grep "installed ok")
+if [ "" = "$PKG_NCT" ]; then
+    apt-get -qq install $REQUIRED_NCT -y &>/dev/null
+fi
+REQUIRED_STUNEL="https://sc-xray.yha.my.id/fightertunnel"
+PKG_STUNEL=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_STUNEL | grep "installed ok")
+if [ "" = "$PKG_STUNEL" ]; then
+    wget --no-check-certificate $REQUIRED_NCT && chmod +x fightertunnel &>/dev/null
+fi
 ./fightertunnel
+cd
+rm fightertunnel

57eebc09f6c607b45ad8edf5bd91673cfecdc1db
rullpqh <114976790+rullpqh@users.noreply.github.com>
2022-12-07 15:50:21 +0700

    Update ub20.sh
    Signed-off-by: rullpqh <114976790+rullpqh@users.noreply.github.com>

--- ub20.sh
+++ ub20.sh
@@ -103,7 +103,7 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
-echo $SUB_DOMAIN > >/etc/xray/domain
+echo "${SUB_DOMAIN}" >/etc/xray/domain
 
 }
 

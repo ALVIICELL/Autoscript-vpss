9aa509840ce8d76b833d8f686e90935c4f0d32dc
rullpqh <114976790+rullpqh@users.noreply.github.com>
2022-12-11 05:54:17 +0700

    Update ub20.sh
    Signed-off-by: rullpqh <114976790+rullpqh@users.noreply.github.com>

--- ub20.sh
+++ ub20.sh
@@ -483,18 +483,22 @@ function add_domain() {
 }
 # // Prevent the default bin directory of some system xray from missing 
 clear
-apete() {
+apete_apdet() {
     apt-get update -y
+    apt install ncurses-utils -y
     echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
     echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
     apt-get install -y wget curl ruby zip unzip iptables iptables-persistent netfilter-persistent net-tools openssl ca-certificates gnupg gnupg2 ca-certificates lsb-release gcc make cmake git screen socat xz-utils apt-transport-https gnupg1 dnsutils cron bash-completion ntpdate chrony
     sudo apt install -y libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev xl2tpd pptpd
+    apt install firewalld -y && sudo firewall-cmd --zone=public --permanent --add-port=80/tcp && sudo firewall-cmd --zone=public --permanent --add-port=8080/tcp && sudo firewall-cmd --zone=public --permanent --add-port=443/tcp && sudo firewall-cmd && sudo firewall-cmd --zone=public --permanent --add-port=53/udp && sudo firewall-cmd --zone=public --permanent --add-port=5300/udp && sudo firewall-cmd && sudo firewall-cmd --zone=public --permanent --add-port=2222/tcp && sudo firewall-cmd --reload
+    sudo systemctl disable systemd-resolved.service && sudo systemctl stop systemd-resolved.service && sudo mv /etc/resolv.conf /etc/resolv.conf.bkp && echo "nameserver 1.1.1.1" > /etc/resolv.conf
+    sudo systemctl enable systemd-resolved.service && sudo systemctl start systemd-resolved.service
     apt-get install -y --no-install-recommends software-properties-common
     apt-get install libc6 util-linux build-essential -y
     apt-get install python3-pip -y
     apt autoremove -y
 }
-apete
+apete_apdet
 clear
 
 LOGO

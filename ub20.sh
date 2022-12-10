321cd9ad1da0f387dea0d5ff20aedfcfb9739f71
rullpqh <114976790+rullpqh@users.noreply.github.com>
2022-12-10 13:06:58 +0700

    Update ub20.sh
    Signed-off-by: rullpqh <114976790+rullpqh@users.noreply.github.com>

--- ub20.sh
+++ ub20.sh
@@ -277,7 +277,8 @@ END
 		[no-haproxy]
 		accept = 445
 		connect = 127.0.0.1:1194
-        END
+		END
+
     apt install squid -y >/dev/null 2>&1
     wget -q -O /etc/squid/squid.conf "${GITHUB_CMD}main/fodder/FighterTunnel-examples/squid.conf"
     wget -q -O /etc/default/dropbear "${GITHUB_CMD}main/fodder/FighterTunnel-examples/dropbear" >/dev/null 2>&1
@@ -478,9 +479,18 @@ function install_sc() {
 function add_domain() {
     read -p "Input Domain :  " domain
     echo $domain >/etc/xray/domain
+	
 }
 # // Prevent the default bin directory of some system xray from missing 
-
+apt-get update -y
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
+apt-get install -y wget curl ruby zip unzip iptables iptables-persistent netfilter-persistent net-tools openssl ca-certificates gnupg gnupg2 ca-certificates lsb-release gcc make cmake git screen socat xz-utils apt-transport-https gnupg1 dnsutils cron bash-completion ntpdate chrony
+sudo apt install -y libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev xl2tpd pptpd
+apt-get install -y --no-install-recommends software-properties-common
+apt-get install libc6 util-linux build-essential -y
+apt-get install python3-pip -y
+apt autoremove -y
 clear
 LOGO
 echo -e "${RED}JANGAN INSTALL SCRIPT INI MENGGUNAKAN KONEKSI VPN!!!${FONT}"

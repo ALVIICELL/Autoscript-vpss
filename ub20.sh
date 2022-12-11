3e6cc345595029fbb75afc2c2d77f3224e812e9b
rullpqh <114976790+rullpqh@users.noreply.github.com>
2022-12-11 07:30:37 +0700

    Update ub20.sh
    Signed-off-by: rullpqh <114976790+rullpqh@users.noreply.github.com>

--- ub20.sh
+++ ub20.sh
@@ -335,6 +335,7 @@ LINUX       : <code>${OS}</code>
     curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
     sed -i "s/xxx/${domain}/g" /var/www/html/index.html >/dev/null 2>&1
     sed -i "s/xxx/${domain}/g" /etc/nginx/conf.d/xray.conf >/dev/null 2>&1
+    sed -i "s/xxx/${domain}/g" /etc/haproxy/haproxy.cfg >/dev/null 2>&1
     sed -i "s/xxx/${MYIP}/g" /etc/squid/squid.conf >/dev/null 2>&1
     sed -i -e 's/\r$//' /usr/bin/get-backres >/dev/null 2>&1
     sed -i -e 's/\r$//' /usr/bin/add-ssh >/dev/null 2>&1
